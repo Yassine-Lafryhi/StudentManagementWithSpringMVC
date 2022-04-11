c77f878266d7062f658a3522923159737e53628a
Yassine Lafryhi <y.lafryhi@gmail.com>
2022-04-11 11:46:19 +0000

    Security configuration (inMemoryAuthentication)

--- src/main/java/com/student/management/StudentManagementApplication.java
+++ src/main/java/com/student/management/StudentManagementApplication.java
@@ -1,7 +1,18 @@
 package com.student.management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class StudentManagementApplication {
@@ -9,5 +20,36 @@ public class StudentManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
     }
+
+    @Configuration
+    @EnableWebSecurity
+    class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            PasswordEncoder passwordEncoder = passwordEncoder();
+            String encodedPWD = passwordEncoder.encode("1234");
+            System.out.println(encodedPWD);
+            auth.inMemoryAuthentication().withUser("user").password(encodedPWD).roles("USER");
+            auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN");
+
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.formLogin();
+            http.authorizeRequests().antMatchers("/").permitAll();
+            http.authorizeRequests().antMatchers("/delete/**", "/edit/**", "/formPatient/**").hasRole("ADMIN");
+            http.authorizeRequests().antMatchers("/index/**").hasRole("USER");
+            http.authorizeRequests().anyRequest().authenticated();
+            http.exceptionHandling().accessDeniedPage("/403");
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+    }
+
 
 }
